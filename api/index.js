60c9f0fca76616cb1b6ecd422372d7f841e0308c
Mert OKAN <mertokan5020@gmail.com>
2023-11-04 11:48:36 +0300

    api folder crated and settings have been made

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log(error.reason)
+  })
+
+const app = express()
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!')
+})
